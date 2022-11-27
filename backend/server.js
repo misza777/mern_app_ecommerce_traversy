7b613169dc12c0be419f0bfe4f00a2e2ef742c55
unknown <miau.nowak@gmail.com>
2022-11-27 14:43:58 +0200

    npm colors in console, set up mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
-const express = require("express");
-const products = require("./data/products");
-const dotenv = require("dotenv");
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+// trzeba dodac .js
+import connectDB from "./config/db.js";
+import colors from "colors";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -22,7 +27,10 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`.yellow
+      .bold
+  )
 );
 
 // app.use((req, res, next) => {
