a137e5b38d18ab05924d38e229c7eaf1cea2a617
micha novakishvili <miau.nowak@gmail.com>
2022-11-30 22:32:30 +0200

    cart screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,18 +1,20 @@
 import React from "react";
 import axios from "axios";
+import { CART_ADD_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
-    dispatch({ type: CART_ADD_ITEM,
+  dispatch({
+    type: CART_ADD_ITEM,
     payload: {
-        product: data._id,
-        name: data.name,
-        image: data.image,
-        price: data.price,
-        countInStock: data.countInStock,
-        qty,
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
     },
-    });
-    //in local storage we can only store strings
-    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  });
+  //in local storage we can only store strings
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,10 +1,46 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useParams, useSearchParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { addToCart } from "../actions/cartActions";
 
 const CartScreen = () => {
+  // id from useParams
   const { id } = useParams();
+  const productId = id;
+  //qty from url
+  const [searchParams, setSearchParams] = useSearchParams();
+  const qty = searchParams.get("qty") ? Number(searchParams.get("qty")) : 1;
 
-  return <div>Cart with id prodct: {id ? id : "nie ma id :("} </div>;
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  return (
+    <div>
+      Cart with id prodct:{" "}
+      {cartItems.map((x, i) => (
+        <div key={i}>
+          {x.name} {x.qty}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CartScreen;
