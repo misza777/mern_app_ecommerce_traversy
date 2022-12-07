2e3e7d4205ddf04945e9099b2e4f9ffeced69194
micha novakishvili <miau.nowak@gmail.com>
2022-12-07 23:27:08 +0200

    cleaning orders nd user details after logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -11,11 +11,13 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_DETAILS_RESET,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_RESET,
 } from "../constants/userConstants";
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -55,6 +57,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -157,14 +161,13 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       type: USER_UPDATE_PROFILE_SUCCESS,
       payload: data,
     });
-//aktualiacja danych usera + local storage
+    //aktualiacja danych usera + local storage
     dispatch({
       type: USER_LOGIN_SUCCESS,
       payload: data,
     });
 
     localStorage.setItem("userInfo", JSON.stringify(data));
-    
   } catch (error) {
     const message =
       error.response && error.response.data.message
